355c8740d2cccd255553f7a35678c1f3dfe7f4e3
Rahul Arora <rahulxxarora@gmail.com>
2020-08-14 15:44:07 +0530

    first commit

--- src/main/java/dao/impl/DataManagerDaoImpl.java
+++ src/main/java/dao/impl/DataManagerDaoImpl.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 public class DataManagerDaoImpl implements DataManagerDao {
 
-    private final Map<String, Map<Long, Metric>> userData;
+    private final Map<String, List<Metric>> userData;
 
     public DataManagerDaoImpl() {
         userData = new HashMap<>();
@@ -21,31 +21,25 @@ public class DataManagerDaoImpl implements DataManagerDao {
 
     @Override
     public void addData(User user, Metric metric) {
-        if (userData.get(user.getUsername()) == null) {
-            Map<Long, Metric> data = new HashMap<>();
-            data.put(metric.getTimestamp(), metric);
-            userData.put(user.getUsername(), data);
+        List<Metric> metrics = userData.get(user.getUsername());
+        if (metrics == null) {
+            metrics = new ArrayList<>();
         }
-        else {
-            Map<Long, Metric> data = userData.get(user.getUsername());
-            data.put(metric.getTimestamp(), metric);
-            userData.put(user.getUsername(), data);
-        }
+        metrics.add(metric);
+        userData.put(user.getUsername(), metrics);
     }
 
     @Override
     public List<Metric> fetchDataByMetricInGivenDuration(User user, MetricType metricType, Long startTime, Long endTime) {
-        Map<Long, ? extends Metric> data = userData.get(user.getUsername());
+        List<Metric> metrics = userData.get(user.getUsername());
 
-        if (data == null || data.isEmpty()) {
+        if (metrics == null || metrics.isEmpty()) {
             return new ArrayList<>();
         }
 
         List<Metric> response = new ArrayList<>();
-        for (Map.Entry<Long, ? extends Metric> entry: data.entrySet()) {
-            Long timestamp = entry.getKey();
-            Metric metric = entry.getValue();
-            if (timestamp < startTime || timestamp > endTime) {
+        for (Metric metric: metrics) {
+            if (metric.getTimestamp() < startTime || metric.getTimestamp() > endTime) {
                 continue;
             }
             if (!metric.getType().equals(metricType)) {
@@ -59,23 +53,20 @@ public class DataManagerDaoImpl implements DataManagerDao {
 
     @Override
     public float fetchMinByMetricInGivenDuration(User user, MetricType metricType, Long startTime, Long endTime) {
-        Map<Long, ? extends Metric> data = userData.get(user.getUsername());
+        List<Metric> metrics = userData.get(user.getUsername());
         Float response = new Float(10000000);
 
-        if (data == null || data.isEmpty()) {
+        if (metrics == null || metrics.isEmpty()) {
             return response;
         }
 
-        for (Map.Entry<Long, ? extends Metric> entry: data.entrySet()) {
-            Long timestamp = entry.getKey();
-            Metric metric = entry.getValue();
-            if (timestamp < startTime || timestamp > endTime) {
+        for (Metric metric: metrics) {
+            if (metric.getTimestamp() < startTime || metric.getTimestamp() > endTime) {
                 continue;
             }
             if (!metric.getType().equals(metricType)) {
                 continue;
             }
-
             if ((Float) metric.getValue() < response) {
                 response = (Float) metric.getValue();
             }
@@ -86,23 +77,20 @@ public class DataManagerDaoImpl implements DataManagerDao {
 
     @Override
     public float fetchMaxByMetricInGivenDuration(User user, MetricType metricType, Long startTime, Long endTime) {
-        Map<Long, ? extends Metric> data = userData.get(user.getUsername());
+        List<Metric> metrics = userData.get(user.getUsername());
         Float response = new Float(0);
 
-        if (data == null || data.isEmpty()) {
+        if (metrics == null || metrics.isEmpty()) {
             return response;
         }
 
-        for (Map.Entry<Long, ? extends Metric> entry: data.entrySet()) {
-            Long timestamp = entry.getKey();
-            Metric metric = entry.getValue();
-            if (timestamp < startTime || timestamp > endTime) {
+        for (Metric metric: metrics) {
+            if (metric.getTimestamp() < startTime || metric.getTimestamp() > endTime) {
                 continue;
             }
             if (!metric.getType().equals(metricType)) {
                 continue;
             }
-
             if ((Float) metric.getValue() > response) {
                 response = (Float) metric.getValue();
             }
@@ -113,24 +101,21 @@ public class DataManagerDaoImpl implements DataManagerDao {
 
     @Override
     public float fetchAvgByMetricInGivenDuration(User user, MetricType metricType, Long startTime, Long endTime) {
-        Map<Long, ? extends Metric> data = userData.get(user.getUsername());
+        List<Metric> metrics = userData.get(user.getUsername());
         float sum = 0;
         int count = 0;
 
-        if (data == null || data.isEmpty()) {
-            return sum;
+        if (metrics == null || metrics.isEmpty()) {
+            return 0;
         }
 
-        for (Map.Entry<Long, ? extends Metric> entry: data.entrySet()) {
-            Long timestamp = entry.getKey();
-            Metric metric = entry.getValue();
-            if (timestamp < startTime || timestamp > endTime) {
+        for (Metric metric: metrics) {
+            if (metric.getTimestamp() < startTime || metric.getTimestamp() > endTime) {
                 continue;
             }
             if (!metric.getType().equals(metricType)) {
                 continue;
             }
-
             sum += (Float) metric.getValue();
             count += 1;
         }
